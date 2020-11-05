ea96ad7cbfd428259d6651d023a53a8b2eb9f55a
ggggabe <who@gabe.is>
2020-11-04 19:18:13 -0500

    containers

--- src/components/containers.js
+++ src/components/containers.js
@@ -1,46 +1,39 @@
-export const Card = ({style, children}) => {
-  return (
-    <div className='rounded' style={{
-      width: 400,
-      height: 400,
-      margin: '40px ',
-      display: 'flex',
-      ...style
-    }}>
-      {children}
-    </div>
-  )
-}
+export const Card = ({style, children}) => (
+  <div className='rounded' style={{
+    width: 400,
+    height: 400,
+    margin: '40px ',
+    display: 'flex',
+    ...style
+  }}>
+    {children}
+  </div>
+)
 
-export const CardHeader = ({meta, header, style}) => {
-  return (
-    <div style={{
-      display: 'flex',
-      flexFlow: 'column',
-      ...style,
-      cursor: 'pointer'
-    }}>
-      <code style={{
-        marginBottom: '10px',
-      }}>{meta}</code>
+export const CardHeader = ({meta, header, style}) => (
+  <div style={{
+    display: 'flex',
+    flexFlow: 'column',
+    ...style,
+    cursor: 'pointer'
+  }}>
+    <code style={{
+      marginBottom: '10px',
+    }}>{meta}</code>
 
-      <h1>{header}</h1>
-    </div>
-  )
-}
+    <h1>{header}</h1>
+  </div>
+)
 
 export const Button = ({
   label,
   fn ,
   style
-}) => {
-  return (
-    <button className='rounded button' style={{
-      padding: '20px 40px',
-      ...style
-    }} onClick={(e) => {
-      e.preventDefault()
-      fn()
-    }}>{label}</button>
-  )
-}
+}) => (
+  <button className='rounded button' style={{ padding: '20px 40px', ...style }} onClick={(e) => {
+    e.preventDefault()
+    fn()
+  }}>
+    {label}
+  </button>
+)
